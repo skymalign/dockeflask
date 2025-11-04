f7bf45f73a608a4cce1a9a06974cde23638ae425
CieloVega <a01741542@tec.mx>
2025-11-04 16:04:23 -0600

    Pequeño cambio

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # Change the message to something unique
-    return jsonify({"message": "it works! Student Cielo"})
+    return jsonify({"message": "it works! Student Cielito"})
 
 if __name__ == "__main__":
     app.run(threaded=True, host='0.0.0.0', port=3000)
